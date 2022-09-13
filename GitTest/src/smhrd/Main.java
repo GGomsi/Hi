d70aed92c6a2ea6c85957b40bc37a77cd799e454
GGomsi <jsuhak@naver.com>
2022-09-13 10:47:59 +0900

    ㅁㄴㅇ

--- GitTest/src/smhrd/Main.java
+++ GitTest/src/smhrd/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//떴나요2?
-		System.out.println("테스트 할거야");
-		System.out.println("또 추가해야지 ㅋㅋ");
+		System.out.println("세번째테스트");
 	}
 
 }
